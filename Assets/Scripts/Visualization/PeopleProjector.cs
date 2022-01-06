1bd862571920561357d39cb22dcc29cb32b54176
DreVinciCode <andre.cleaver@tufts.edu>
2022-01-06 16:11:26 -0500

    Added people_msgs using ROS sharp feature

---
+++ Assets/Scripts/Visualization/PeopleProjector.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class PeopleProjector : MonoBehaviour
+    {
+        public Vector3 Offset;
+
+        [SerializeField]
+        private ParticleSystem _localizationParticles;
+
+        private ParticleSystem.Particle[] _particles;
+        private bool _isMessageReceived;
+
+        private MessageTypes.People.PositionMeasurementArray[] _people;
+        private int _totalCount;
+        private float[] _coocurrence;
+
+
+        private void Update()
+        {
+            if (_isMessageReceived)
+                ProcessMessage();
+        }
+
+        public void Write(MessageTypes.People.PositionMeasurementArray message)
+        {
+            _totalCount = message.people.Length;
+            _coocurrence = message.cooccurrence;
+
+        }
+
+        private void ProcessMessage()
+        {
+
+        }
+
+        private Vector3 GetPosition(MessageTypes.Nav.OccupancyGrid message)
+        {
+            return new Vector3(
+                (float)message.info.origin.position.x,
+                (float)message.info.origin.position.y,
+                (float)message.info.origin.position.z);
+        }
+
+        private Quaternion GetRotation(MessageTypes.Nav.OccupancyGrid message)
+        {
+            return new Quaternion(
+                (float)message.info.origin.orientation.x,
+                (float)message.info.origin.orientation.y,
+                (float)message.info.origin.orientation.z,
+                (float)message.info.origin.orientation.w);
+        }
+    }
+}
