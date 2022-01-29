ec50c81549fd6d508b81c800d532d1b2271358df
DreVinciCode <dre.vivero@gmail.com>
2022-01-29 15:35:56 -0500

    fixed polygon renderer

--- Assets/Scripts/Visualization/PolygonProjector.cs
+++ Assets/Scripts/Visualization/PolygonProjector.cs
@@ -1,15 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace RosSharp.RosBridgeClient
 {
     public class PolygonProjector : MonoBehaviour
     {
+        [SerializeField] LineRenderer lineRenderer;
         private bool _isMessageReceived;
         private MessageTypes.Geometry.Polygon _polygon;
         private MessageTypes.Geometry.Point32[] _points;
         private int _totalPoints;
+
+        private void Start()
+        {
+            lineRenderer.startWidth = 0.005f;
+        }
 
         private void Update()
         {
@@ -27,11 +31,19 @@ namespace RosSharp.RosBridgeClient
 
         private void ProcessMessage()
         {
-            //find a way to display a closed polygon with the given points. 
-            // round the points in order to check for any differences. 
+            lineRenderer.positionCount = _totalPoints + 1;
+            Vector3[] waypoints = new Vector3[_totalPoints + 1];
 
-            Debug.Log("Total points: " + _totalPoints);
+            for (int i = 0; i < _totalPoints; i++)
+            {
+                var vertex = _points[i];
+                var position = new Vector3((float)vertex.x, (float)vertex.y, (float)vertex.z).Ros2Unity();
+                waypoints[i] = position;
+            }
 
+            waypoints[_totalPoints] = new Vector3((float)_points[0].x, (float)_points[0].y, (float)_points[0].z).Ros2Unity();
+            lineRenderer.SetPositions(waypoints);
+            lineRenderer.enabled = true;
             _isMessageReceived = false;
         }
     }
