39d07ad8edd22e208248f75eb00a3f55c53242d4
DreVinciCode <dre.vivero@gmail.com>
2022-02-05 00:23:26 -0500

    Added multi target selection

--- Assets/Scripts/Submenu_scene.cs
+++ Assets/Scripts/Submenu_scene.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Submenu_scene : MonoBehaviour
 {
     public GameObject IPAddressPanel;
+    public GameObject TargetCubePanel;
     public GameObject SensorPanel;
 
     private bool _toggleIPAddressPanel = false;
     private bool _toggleSensorsPanel = false;
+    private bool _toggleTargetCubePanel = false;
 
     private void Start()
     {
         IPAddressPanel.SetActive(_toggleIPAddressPanel);
         SensorPanel.SetActive(_toggleSensorsPanel);
+        TargetCubePanel.SetActive(_toggleTargetCubePanel);
     }
 
     public void ToggleIPAddressPanel()
@@ -31,6 +34,16 @@ public class Submenu_scene : MonoBehaviour
         _toggleSensorsPanel = !_toggleSensorsPanel;
         SensorPanel.SetActive(_toggleSensorsPanel);
         if(_toggleSensorsPanel)
+        {
+            SensorPanel.transform.parent.transform.SetAsLastSibling();
+        }
+    }
+
+    public void ToggleTargetCubePanel()
+    {
+        _toggleTargetCubePanel = !_toggleTargetCubePanel;
+        TargetCubePanel.SetActive(_toggleTargetCubePanel);
+        if (_toggleTargetCubePanel)
         {
             SensorPanel.transform.parent.transform.SetAsLastSibling();
         }
