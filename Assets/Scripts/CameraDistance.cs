789ca443927143f46d536404c3af8f557d0df9c3
DreVinciCode <dre.vivero@gmail.com>
2022-02-13 14:28:53 -0500

    calculated distance with direction from AR camera to target image

---
+++ Assets/Scripts/CameraDistance.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using Vuforia;
+
+
+public class CameraDistance : MonoBehaviour
+{
+    public ObserverBehaviour observerBehaviour;
+    public ImageTargetBehaviour imageTargetBehaviour;
+    public Transform parent;
+    public GameObject markerObject;
+    private Vector3 _distance;
+
+    private float _safetydistance = 0.1f;
+
+
+    private void Update()
+    {
+        if (observerBehaviour.TargetStatus.Status == Status.TRACKED)
+        {
+            _distance = (Camera.main.transform.position - imageTargetBehaviour.transform.position);
+            Vector3 projectOnPlane = Vector3.ProjectOnPlane(_distance, Vector3.up);
+            var magnitude = projectOnPlane.magnitude;
+            var _distanceMinusSD = magnitude - _safetydistance;
+
+
+            var direction = projectOnPlane.normalized;
+
+            var newVector3 = imageTargetBehaviour.transform.position + direction * _distanceMinusSD;
+            markerObject.transform.position = newVector3;
+        }
+    }
+}
