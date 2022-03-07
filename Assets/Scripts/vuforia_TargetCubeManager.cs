840ab07c93e5d57e63b9014635f44091cacfd2a1
DreVinciCode <andre.cleaver@tufts.edu>
2022-03-07 13:44:22 -0500

    added robot name to battery panel

--- Assets/Scripts/vuforia_TargetCubeManager.cs
+++ Assets/Scripts/vuforia_TargetCubeManager.cs
@@ -1,17 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using Vuforia;
+using TMPro;
 
 public class vuforia_TargetCubeManager : MonoBehaviour
 {
     public GameObject TargetCube;
+    public TMP_Text TargetCubeName;
     private MultiTargetBehaviour[] _targetCubes;
 
     public void SelectCube(Button button)
     {
         var name = button.name;
+        //Find better way to do this; ideally from a rostopic
+        var robotname = name.Replace("_Cube", "");
+        TargetCubeName.text = "Robot: " + robotname; 
         _targetCubes = TargetCube.GetComponents<MultiTargetBehaviour>();
 
         for (int i = 0; i < _targetCubes.Length; i++)
